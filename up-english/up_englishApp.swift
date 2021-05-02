9920bee59f99ec24a1b7e14c455359c901b6f827
James Tsai <jamesqiyucai@icloud.com>
2021-05-02 19:57:57 +0800

    Initial Commit

---
+++ up-english/up_englishApp.swift
@@ -0,0 +1,17 @@
+//
+//  up_englishApp.swift
+//  up-english
+//
+//  Created by James Tsai on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct up_englishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
