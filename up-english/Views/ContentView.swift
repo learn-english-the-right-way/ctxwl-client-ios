5a8605f7ebc5ad941dd7577f7c7975118265b784
James Cai <jamescaiqiyu@icloud.com>
2022-01-18 00:23:27 +0800

    restored navigation logic, pending home page init in content view

--- up-english/Views/ContentView.swift
+++ up-english/Views/ContentView.swift
@@ -19,88 +19,15 @@ struct ContentView<RegistrationServiceType, UserServiceType>: View where Registr
     @State var test: UITextRange? = nil
     
     var body: some View {
-//        switch viewRouter.currentPage {
-//        case .Registration:
-//            Registration(model: RegistrationModelDefault(registrationService: self.registrationService, userService: self.userService, viewRouter: self.viewRouter))
-//        case .Login:
-//            Login(LoginModelDefault(userService: self.userService, viewRouter: self.viewRouter))
-//        case .Confirmation:
-//            EmailVerification(model: EmailVerificationModelDefault(registrationService: self.registrationService, userService: self.userService, viewRouter: self.viewRouter))
-//        }
-//                    ArticleReaderViewRepresentable(text: "not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje not suitable for children under 15 years of age or pregnant soiejfosjfe soijeosiejf soeijfosiejfsoe soiejfosiejfo osiejfosijefoisjef soeijfosiejf soijefoisjefos osijeofisjef soijefosijefosjf soeijfosihefosef oisjefosijeof soiehfosefose siefjesif soeijfosejf soeijfosefj osijeofsje", range: $test)
-//                    Text("\(test?.start ?? UITextPosition()), \(test?.end ?? UITextPosition())")
-//                    Button("clear") {
-//                        test = nil
-//                    }
-        ScrollViewRepresentable {
-            VStack {
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-            }
-            VStack {
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-            }
-            VStack {
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-            }
-            VStack {
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-                Text("fuckfuck")
-                Button("wowowo") {
-                }
-            }
+        switch viewRouter.currentPage {
+        case .Registration:
+            Registration(model: RegistrationModelDefault(registrationService: self.registrationService, userService: self.userService, viewRouter: self.viewRouter))
+        case .Login:
+            Login(LoginModelDefault(userService: self.userService, viewRouter: self.viewRouter))
+        case .Confirmation:
+            EmailVerification(model: EmailVerificationModelDefault(registrationService: self.registrationService, userService: self.userService, viewRouter: self.viewRouter))
+        case .ArticleList:
+            // add main page initialization logic
         }
     }
 }
